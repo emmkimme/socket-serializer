7f53460f58c256b37ae8cab1d42e13edbb5425e7
emmanuel.kimmerlin@thomsonreuters.com <emmanuel.kimmerlin@thomsonreuters.com>
2018-04-05 18:21:58 +0200

    Fix filename case

--- src/socket-serializer.ts
+++ src/socket-serializer.ts
@@ -7,13 +7,13 @@ export {IpcPacketBufferWrap} from './code/ipcPacketBufferWrap';
 export {BufferType} from './code/ipcPacketBufferWrap';
 
 export { Writer } from './code/writer';
-export { BufferWriter } from './code/BufferWriter';
-export { BufferListWriter } from './code/BufferListWriter';
-export { SocketWriter } from './code/SocketWriter';
-export { DelayedSocketWriter, BufferedSocketWriter } from './code/SocketBufferWriter';
+export { BufferWriter } from './code/bufferWriter';
+export { BufferListWriter } from './code/bufferListWriter';
+export { SocketWriter } from './code/socketWriter';
+export { DelayedSocketWriter, BufferedSocketWriter } from './code/socketBufferWriter';
 
 export { Reader } from './code/reader';
-export { BufferReader } from './code/BufferReader';
-export { BufferListReader } from './code/BufferListReader';
+export { BufferReader } from './code/bufferReader';
+export { BufferListReader } from './code/bufferListReader';
 
 
