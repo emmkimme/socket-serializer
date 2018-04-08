f7381f594c1cdf81e7cf37802f2bc1d8ccb67825
emmanuel.kimmerlin@thomsonreuters.com <emmanuel.kimmerlin@thomsonreuters.com>
2018-04-08 10:28:38 +0200

    Indentation

--- src/socket-serializer.ts
+++ src/socket-serializer.ts
@@ -1,7 +1,7 @@
 
-export {IpcPacketBuffer} from './code/ipcPacketBuffer';
-export {IpcPacketBufferWrap} from './code/ipcPacketBufferWrap';
-export {BufferType} from './code/ipcPacketBufferWrap';
+export { IpcPacketBuffer } from './code/ipcPacketBuffer';
+export { IpcPacketBufferWrap } from './code/ipcPacketBufferWrap';
+export { BufferType } from './code/ipcPacketBufferWrap';
 
 export { Writer } from './code/writer';
 export { BufferWriter } from './code/bufferWriter';
@@ -11,10 +11,10 @@ export { Reader } from './code/reader';
 export { BufferReader } from './code/bufferReader';
 export { BufferListReader } from './code/bufferListReader';
 
-export {IpcPacketSerializer, IpcPacketParser} from './code/ipcPacket';
+export { IpcPacketSerializer, IpcPacketParser}  from './code/ipcPacket';
 
-export {IpcNet} from './code/ipcNet';
-export {IpcPacketNet} from './code/ipcPacketNet';
+export { IpcNet } from './code/ipcNet';
+export { IpcPacketNet } from './code/ipcPacketNet';
 export { SocketWriter } from './code/socketWriter';
 export { DelayedSocketWriter, BufferedSocketWriter } from './code/socketBufferWriter';
 
