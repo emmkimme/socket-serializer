ea983d7a2dc2fc3f1b8e0b614e19a36e2adbbe6e
emmanuel.kimmerlin@thomsonreuters.com <emmanuel.kimmerlin@thomsonreuters.com>
2018-09-14 22:02:30 +0200

    Remove the optimized object serialization

--- src/code/ipcPacketBufferWrap.ts
+++ src/code/ipcPacketBufferWrap.ts
@@ -52,8 +52,8 @@ export class IpcPacketBufferWrap {
     protected _headerSize: number;
 
     writeArray: Function = this.writeArrayWithSize;
-    // writeObject: Function = this.writeObjectSTRINGIFY;
-    writeObject: Function = this.writeObjectDirect;
+    writeObject: Function = this.writeObjectSTRINGIFY;
+    // writeObject: Function = this.writeObjectDirect;
 
     constructor() {
         this._type = BufferType.NotValid;
